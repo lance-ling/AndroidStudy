b92ef4ba3af7d2e581145effe32c6cd6f22335e9
lingsh <770833105@qq.com>
2019-10-26 15:39:32 +0800

    lession 02 简单的地理问答app 挑战练习 1. 为TextView添加监听器 2. 添加回退按钮

---
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day02/GeoQuiz2Challenge/app/src/main/java/com/lingsh/android/geoquiz2challenge/MainActivity.java
@@ -0,0 +1,98 @@
+package com.lingsh.android.geoquiz2challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private Button mPrevButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_gd, true),
+            new Question(R.string.question_animal, false),
+            new Question(R.string.question_hn, false),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_sh, false)
+    };
+
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        updateQuestion();
+        // 给TextView添加监听器 用户点击文字区域时, 可以跳转到下一题
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+
+            }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean questionAnswer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int toastTexId;
+        if (questionAnswer == userPressedTrue) {
+            toastTexId = R.string.correct_toast;
+        } else {
+            toastTexId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(MainActivity.this, toastTexId, Toast.LENGTH_SHORT).show();
+    }
+}
