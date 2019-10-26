63e011a63e627f746dbf549f061db0d8f316fe65
lingsh <770833105@qq.com>
2019-10-26 14:40:41 +0800

    lession 02 简单的地理问答app (MVC 架构(Question.java, activity.xml, Activity.java) 复数的题目及不同的答案 题目切换)

---
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day02/GeoQuiz2/app/src/main/java/com/lingsh/android/geoquiz/MainActivity.java
@@ -0,0 +1,80 @@
+package com.lingsh.android.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_gd, true),
+            new Question(R.string.question_animal, false),
+            new Question(R.string.question_hn, false),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_sh, false)
+    };
+
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        updateQuestion();
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+
+            }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean questionAnswer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int toastTexId;
+        if (questionAnswer == userPressedTrue) {
+            toastTexId = R.string.correct_toast;
+        } else {
+            toastTexId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(MainActivity.this, toastTexId, Toast.LENGTH_SHORT).show();
+    }
+}
