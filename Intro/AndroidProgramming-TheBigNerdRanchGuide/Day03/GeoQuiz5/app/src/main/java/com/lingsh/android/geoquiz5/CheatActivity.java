54cd59364518422c4becb695f53f5ac2d0f1463d
lingsh <770833105@qq.com>
2019-10-29 15:52:33 +0800

    lession 05 简单的地理问答app bug查找
    问题所在: 在横屏的时候, mShowAnswerButton按钮的属性被初始化为0

--- Intro/AndroidProgramming-TheBigNerdRanchGuide/Day03/GeoQuiz5/app/src/main/java/com/lingsh/android/geoquiz5/CheatActivity.java
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day03/GeoQuiz5/app/src/main/java/com/lingsh/android/geoquiz5/CheatActivity.java
@@ -39,8 +39,8 @@ public class CheatActivity extends AppCompatActivity {
 
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
-    private TextView mCurrentApiLevel;
-    private TextView mRemainCheatCnt;
+    private TextView mCurrentApiLevelView;
+    private TextView mRemainCheatCntView;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue, int mCheatCount) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -58,10 +58,8 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        // 移动到前面 是因为updateCheatView() 需要使用到该变量
-        mAnswerTextView = findViewById(R.id.answer_text_view);
-        mRemainCheatCnt = findViewById(R.id.remain_cheat_cnt_view);
-        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        // 移动到前面 是因为updateCheatView() 需要使用到这些组件
+        initBindActivityView();
 
         // 用于解决 旋转CheatActivity清除作弊痕迹
         if (savedInstanceState != null) {
@@ -71,22 +69,34 @@ public class CheatActivity extends AppCompatActivity {
             }
         }
 
+        // 获取父Activity传入的参数
+        // 作弊: 该问题的答案
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        // 剩余作弊次数显示
+        mCheatCount = getIntent().getIntExtra(EXTRA_COUNT_CHEAT, 0);
 
+        // 组件的操作
+        // 显示答案按钮 点击监听
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAnswerIsShown = true;
                 updateCheatView();
+
             }
         });
 
-        mCurrentApiLevel = findViewById(R.id.current_api_level_view);
-        mCurrentApiLevel.setText(mCurrentApiLevelStr);
+        mCurrentApiLevelView.setText(mCurrentApiLevelStr);
 
-        // 剩余作弊次数显示
-        mCheatCount = getIntent().getIntExtra(EXTRA_COUNT_CHEAT, 0);
+        // 页面的更新 (一次)
         updateRemainCheatTime();
+    }
+
+    private void initBindActivityView() {
+        mAnswerTextView = findViewById(R.id.answer_text_view);
+        mRemainCheatCntView = findViewById(R.id.remain_cheat_cnt_view);
+        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        mCurrentApiLevelView = findViewById(R.id.current_api_level_view);
     }
 
     @Override
@@ -97,24 +107,33 @@ public class CheatActivity extends AppCompatActivity {
     }
 
     private void updateCheatView() {
+        // 显示答案
+        showQuestionAnswer();
+
+        // 隐藏按钮
+        hideCheatButton();
+
+        // 更新剩余作弊次数
+        updateRemainCheatTime();
+    }
+
+    private void showQuestionAnswer() {
         if (mAnswerIsTrue) {
             mAnswerTextView.setText(R.string.true_button);
         } else {
             mAnswerTextView.setText(R.string.false_button);
         }
         setAnswerShownResult(true);
-
-        hideCheatButton();
-
-        updateRemainCheatTime();
     }
 
     private void updateRemainCheatTime() {
         int remain_cnt = mCheatCountMax - mCheatCount;
+
+        // 是否已经作弊 显示过答案
         if (mAnswerIsShown) {
-            mRemainCheatCnt.setText("剩余作弊次数: " + (--remain_cnt));
+            mRemainCheatCntView.setText("剩余作弊次数: " + (--remain_cnt));
         } else {
-            mRemainCheatCnt.setText("剩余作弊次数: " + remain_cnt);
+            mRemainCheatCntView.setText("剩余作弊次数: " + remain_cnt);
         }
         Log.d(TAG, "updateRemainCheatTime mCheatCnt=" + mCheatCount + " remain_cnt=" + remain_cnt);
 
@@ -129,6 +148,7 @@ public class CheatActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // 添加一个 API 21级(Lollipop)版本的代码
             // 在隐藏按钮的同时显示一段特效动画
+            // bugs: 在横屏的时候mShowAnswerButton的属性被初始化为0
             int cx = mShowAnswerButton.getWidth() / 2;
             int cy = mShowAnswerButton.getHeight() / 2;
             float radius = mShowAnswerButton.getWidth();
