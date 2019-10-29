c22b2cfd692463b61855efa7ad09fe5b203e8006
lingsh <770833105@qq.com>
2019-10-29 21:41:41 +0800

    lesson 01 陋习记录app Fragment使用 通过AppCompatActivity显示Fragment 使用FragmentManager管理

--- Intro/AndroidProgramming-TheBigNerdRanchGuide/Day04/CriminalIntent/app/src/main/java/com/lingsh/android/criminalintent/CrimeFragment.java
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day04/CriminalIntent/app/src/main/java/com/lingsh/android/criminalintent/CrimeFragment.java
@@ -1,7 +1,76 @@
 package com.lingsh.android.criminalintent;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // 实例化fragment视图的布局
+        // 直接调用LayoutInflater.inflate(...)方法生成fragment视图
+        // 1.布局的资源id 2.视图的父视图 3.布局生成器是否将生成的视图添加到父视图(false, 表示以代码的方式手动添加)
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        // 同一绑定组件
+        mTitleField = view.findViewById(R.id.crime_title);
+        mDateButton = view.findViewById(R.id.crime_date);
+        mSolvedCheckBox = view.findViewById(R.id.crime_solved);
+
+        // 对组件进行操作
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // 故意留下空白
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // 故意留下空白
+            }
+        });
+
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return view;
+    }
+
 
 }
