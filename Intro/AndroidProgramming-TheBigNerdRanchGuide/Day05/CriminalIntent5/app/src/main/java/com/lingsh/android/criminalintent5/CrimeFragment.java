153857e1519216e76f1599cfee849299a10ed709
lingsh <770833105@qq.com>
2019-10-30 21:33:20 +0800

    lesson 05 陋习记录app 初始代码

---
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day05/CriminalIntent5/app/src/main/java/com/lingsh/android/criminalintent5/CrimeFragment.java
@@ -0,0 +1,92 @@
+package com.lingsh.android.criminalintent5;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CrimeFragment extends Fragment {
+
+    private static final String ARG_CRIME_ID = "crime_id";
+
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        // setArguments()方法必须在fragment创建后, 添加给activity之前完成
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UUID crimeId = (UUID) Objects.requireNonNull(getArguments()).getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // 实例化fragment视图的布局
+        // 直接调用LayoutInflater.inflate(...)方法生成fragment视图
+        // 1.布局的资源id 2.视图的父视图 3.布局生成器是否将生成的视图添加到父视图(false, 表示以代码的方式手动添加)
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        // 同一绑定组件
+        mTitleField = view.findViewById(R.id.crime_title);
+        mDateButton = view.findViewById(R.id.crime_date);
+        mSolvedCheckBox = view.findViewById(R.id.crime_solved);
+
+        // 对组件进行操作
+        mTitleField.setText(mCrime.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // 故意留下空白
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // 故意留下空白
+            }
+        });
+
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return view;
+    }
+}
---
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day05/CriminalIntent5/app/src/main/java/com/lingsh/android/criminalintent5/CrimeListFragment.java
@@ -0,0 +1,210 @@
+package com.lingsh.android.criminalintent5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mCrimeAdapter;
+    // case: "Friday, Jul 22, 2016"
+    private String inFormat = "EE, MMM dd, yyyy";
+    // 当前更新列表项位置
+    private int position = -1;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+
+        return view;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // 此处使用resume而不是start
+        // 是因为如果前面的activity是透明的, 那么本页面可能只是在暂停状态, 不会触发start
+        // 而resume是返回页面必然触发
+        // 实现从详细页面返回后直接刷新列表信息
+        updateUI();
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        if (mCrimeAdapter == null) {
+            mCrimeAdapter = new CrimeAdapter(crimes);
+            // 关联RecyclerView和Adapter
+            mCrimeRecyclerView.setAdapter(mCrimeAdapter);
+        } else {
+            // 实现高效的RecyclerView刷新
+            // notifyDataSetChanged()方法会通知RecyclerView刷新全部的可见列表项
+            // mCrimeAdapter.notifyDataSetChanged();
+            // 因为返回列表页面时, 最多可能只有一个列表项会发生变化
+            // notifyItemChanged()可以指定刷新具体位置的列表项
+            if (position != -1) {
+                mCrimeAdapter.notifyItemChanged(position);
+            } else {
+                mCrimeAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+    public abstract class BaseCrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        protected Crime mCrime;
+        protected TextView mTitleTextView;
+        protected TextView mDateTextView;
+
+        public BaseCrimeHolder(LayoutInflater inflater, ViewGroup parent, int resource) {
+            super(inflater.inflate(resource, parent, false));
+
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = itemView.findViewById(R.id.crime_title);
+            mDateTextView = itemView.findViewById(R.id.crime_date);
+            initialComponents();
+        }
+
+        /**
+         * 让子类初始化自己独有的组件
+         */
+        protected abstract void initialComponents();
+
+        /**
+         * 组件内容绑定 让子类自己实现
+         */
+        protected abstract void bind(Crime crime);
+
+        @Override
+        public void onClick(View v) {
+            // 获得该ItemView的位置
+            position = mCrimeRecyclerView.getChildAdapterPosition(v);
+            // 从Fragment调用Activity
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
+        }
+    }
+
+    private class CrimeHolder extends BaseCrimeHolder {
+
+        private ImageView mSolvedImageView;
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater, parent, R.layout.list_item_crime);
+        }
+
+        @Override
+        protected void initialComponents() {
+            // 一次性 实例化相关组件
+            mSolvedImageView = itemView.findViewById(R.id.crime_solved);
+        }
+
+        @Override
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(DateFormat.format(inFormat, mCrime.getDate()));
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    private class CrimeRequiredPoliceHolder extends BaseCrimeHolder {
+        private Button mCrimeRequiredPoliceButton;
+
+        public CrimeRequiredPoliceHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater, parent, R.layout.list_item_crime_required_police);
+        }
+
+        @Override
+        protected void initialComponents() {
+            mCrimeRequiredPoliceButton = itemView.findViewById(R.id.crime_required_police_button);
+
+            mCrimeRequiredPoliceButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), R.string.called_police, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(DateFormat.format(inFormat, mCrime.getDate()));
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter {
+
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @Override
+        public BaseCrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            // 根据getItemViewType获得类型分别返回不同视图
+            if (viewType == 0) {
+                return new CrimeHolder(layoutInflater, parent);
+            } else {
+                return new CrimeRequiredPoliceHolder(layoutInflater, parent);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            // 绑定相应的Crime实例
+            Crime crime = mCrimes.get(position);
+
+            // 根据得到的不同item view type 绑定不同的Fragment视图
+            if (getItemViewType(position) == 0) {
+                ((CrimeHolder) holder).bind(crime);
+            } else {
+                ((CrimeRequiredPoliceHolder) holder).bind(crime);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            Crime crime = mCrimes.get(position);
+            // 0:不需要 1:需要
+            return crime.getRequiresPolice();
+        }
+    }
+}
