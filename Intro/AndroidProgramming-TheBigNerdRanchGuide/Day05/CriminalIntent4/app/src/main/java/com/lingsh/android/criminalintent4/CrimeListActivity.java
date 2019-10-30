286925797f6e7a53ef57c696a4401c7b013dd8a1
lingsh <770833105@qq.com>
2019-10-30 20:12:14 +0800

    lesson 04 陋习记录app 初始代码

---
+++ Intro/AndroidProgramming-TheBigNerdRanchGuide/Day05/CriminalIntent4/app/src/main/java/com/lingsh/android/criminalintent4/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.lingsh.android.criminalintent4;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
